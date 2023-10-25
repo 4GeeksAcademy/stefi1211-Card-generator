739880bffd7df4773868c69a1a1cefd4d16e94bb
Stefi <144570099+stefi1211@users.noreply.github.com>
2023-10-25 23:55:11 +0000

    my code

--- src/app.js
+++ src/app.js
@@ -1,11 +1,83 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const palos = ["corazon", "diamante", "pica", "trebol"];
+const valores = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+let paloArriba = document.querySelector("#simboloArriba");
+let numero = document.querySelector("#numero");
+let paloAbajo = document.querySelector("#simboloAbajo");
+
+let cardWidthInput = document.querySelector("#cardWidth");
+let cardHeightInput = document.querySelector("#cardHeight");
+
+function eleccionCarta(arreglo1, arreglo2) {
+  let carta = [];
+  carta.push(arreglo1[Math.floor(Math.random() * arreglo1.length)]);
+  carta.push(arreglo2[Math.floor(Math.random() * arreglo2.length)]);
+  return carta;
+}
+function asignarColor(arreglo) {
+  if (arreglo[0] === "corazon" || arreglo[0] === "diamante") {
+    paloArriba.style.color = "red";
+    numero.style.color = "red";
+    paloAbajo.style.color = "red";
+  } else {
+    paloArriba.style.color = "black";
+    numero.style.color = "black";
+    paloAbajo.style.color = "black";
+  }
+}
+function asignarPalo(arreglo) {
+  if (arreglo[0] === "corazon") {
+    paloArriba.innerHTML = "♥";
+    paloAbajo.innerHTML = "♥";
+  } else if (arreglo[0] === "diamante") {
+    paloArriba.innerHTML = "♦";
+    paloAbajo.innerHTML = "♦";
+  } else if (arreglo[0] === "trebol") {
+    paloArriba.innerHTML = "♣";
+    paloAbajo.innerHTML = "♣";
+  } else {
+    paloArriba.innerHTML = "♠";
+    paloAbajo.innerHTML = "♠";
+  }
+}
+function asignarValor(arreglo) {
+  numero.innerHTML = arreglo[1];
+}
+
+function generacionCarta() {
+  let carta = eleccionCarta(palos, valores);
+  asignarColor(carta);
+  asignarPalo(carta);
+  asignarValor(carta);
+
+  const width = parseInt(cardWidthInput.value) / 300;
+  const height = parseInt(cardHeightInput.value) / 450;
+
+  document.querySelector(
+    "#carta"
+  ).style.transform = `scale(${width}, ${height})`;
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generacionCarta();
 };
+
+document.querySelector(".boton").addEventListener("click", generacionCarta);
+
+setInterval(generacionCarta, 10000);
